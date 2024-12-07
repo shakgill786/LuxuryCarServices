9158836cee029087f1eaae08cc23078d96d1f28c
shakgill786 <sgillani1@gmail.com>
2024-12-07 09:39:40 -0500

    frontend

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,8 +1,7 @@
 const { sequelize } = require('./db/models');
 
-
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-  if (!data.includes(process.env.SCHEMA)) {
+  if (process.env.NODE_ENV === 'production' && !data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
   }
 });
